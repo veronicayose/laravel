2234d315a9e0f4a9f0d33ccc488cd0f68d16289d
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2021-09-10 09:55:29 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/motor/{jenis?}',function($jenis='Sport'){
+    return "Motor dengan jenis ".$jenis;
+});
